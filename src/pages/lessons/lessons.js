3160d084551293ed756ed0bddeb5ebb56c376f6c
AbuniDaniel <daniel.abuni@gmail.com>
2023-11-11 21:29:40 +0200

    .

--- src/pages/lessons/lessons.js
+++ src/pages/lessons/lessons.js
@@ -1,54 +1,65 @@
-import "./lessons.css";
-import { authContext } from "../../helpers/authContext"
-import Menu from "../../fragments/menu/menu"
-import { useContext, useState } from "react";
-import { Button } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useContext, useState } from "react";
+import { Upload, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { authContext } from "../../helpers/authContext";
+import Menu from "../../fragments/menu/menu";
+import axios from 'axios';
+
 function Lessons() {
   const { authState } = useContext(authContext);
+  const [fileList, setFileList] = useState([]);
 
-  const [loadings, setLoadings] = useState([]);
-  const enterLoading = (index) => {
-    setLoadings((prevLoadings) => {
-      const newLoadings = [...prevLoadings];
-      newLoadings[index] = true;
-      return newLoadings;
-    });
-    setTimeout(() => {
-      setLoadings((prevLoadings) => {
-        const newLoadings = [...prevLoadings];
-        newLoadings[index] = false;
-        return newLoadings;
-      });
-    }, 2000);
+  const handleChange = info => {
+    let newFileList = [...info.fileList];
+    setFileList(newFileList);
   };
+
+  const customRequest = async (options) => {
+    const formData = new FormData();
+    formData.append('file', options.file);
+
+    try {
+      const response = await axios.post('http://localhost:3001/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      message.success('Upload successful');
+      options.onSuccess(response.data, options.file);
+    } catch (error) {
+      message.error('Upload failed');
+      options.onError(error);
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      <PlusOutlined />
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
 
   return (
     <div>
       <Menu />
-      {authState.status ?
+      {authState.status ? (
         <>
           <p>iesti logat pe contul {authState.username}</p>
-          <Button
-          type="primary"
-          icon={<UploadOutlined />}
-          loading={loadings[1]}
-          onClick={() => enterLoading(1)}
-        >
-          Click me!
-        </Button>
-
+          <Upload
+            listType="picture-circle"
+            fileList={fileList}
+            onChange={handleChange}
+            accept=".pdf"
+            customRequest={customRequest}
+          >
+            {fileList.length >= 1 ? null : uploadButton}
+          </Upload>
         </>
-        :
-        <>
-
-          <p>nu iesit logat</p>
-        </>
-      }
+      ) : (
+        <p>nu iesit logat</p>
+      )}
     </div>
-
   );
-
 }
 
 export default Lessons;
